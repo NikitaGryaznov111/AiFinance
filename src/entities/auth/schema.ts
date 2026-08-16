28c40d417ddd47d93ebf4ef36127a61901d93c5d
Nikita <nikitagryaznov1997@gmail.com>
2026-08-16 12:09:34 +0700

    feat: - Добавлена zod схема для auth

---
+++ src/entities/auth/schema.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+const emailSchema = z.email({ error: 'Введите корректный email' }).trim().toLowerCase();
+
+const passwordSchema = z.string().min(6, { error: 'Пароль должен быть не менее 6 символов' });
+
+export const signInSchema = z.object({
+  emil: emailSchema,
+  password: passwordSchema,
+});
+
+export const signUpSchema = z
+  .object({
+    email: emailSchema,
+    password: passwordSchema,
+    confirmPassword: z.string(),
+  })
+  .refine((value) => value.password === value.confirmPassword, {
+    path: ['confirmPassword'],
+    error: 'Пароли не совпадают',
+  });
+
+export type TSignInInput = z.infer<typeof signInSchema>;
+export type TSignUpInput = z.infer<typeof signUpSchema>;
